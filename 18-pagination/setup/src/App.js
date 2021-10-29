e06fb948913ba2889308a9c6ee9ca19e1978991e
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2021-10-29 23:37:53 +1300

    project 18 complete

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,74 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
-  return <h2>pagination starter</h2>
+	const [pageIndex, setPageIndex] = useState(0)
+	const [followers, setFollowers] = useState([])
+	const { loading, data } = useFetch()
+
+	useEffect(() => {
+		if (!loading) setFollowers(data[pageIndex])
+	}, [loading, pageIndex, data])
+
+	return (
+		<main>
+			<section className='section-title'>
+				<h1>{loading ? 'loading...' : 'pagination'}</h1>
+				<div className='underline'></div>
+			</section>
+			<section className='followers'>
+				<div className='container'>
+					{followers.map((follower) => (
+						<Follower key={follower.id} {...follower} />
+					))}
+				</div>
+				{!loading && (
+					<div className='btn-container'>
+						<button
+							className='prev-btn'
+							onClick={() => setPageIndex(0)}
+							disabled={pageIndex <= 0}
+						>
+							{'<<'}
+						</button>
+						<button
+							className='prev-btn'
+							onClick={() => setPageIndex((prev) => prev - 1)}
+							disabled={pageIndex <= 0}
+						>
+							{'<'}
+						</button>
+						{data.map((_, index) => (
+							<button
+								key={index}
+								className={`page-btn ${
+									index === pageIndex ? 'active-btn' : ''
+								}`}
+								onClick={() => setPageIndex(index)}
+							>
+								{index + 1}
+							</button>
+						))}
+						<button
+							className='next-btn'
+							onClick={() => setPageIndex((prev) => prev + 1)}
+							disabled={pageIndex >= data.length - 1}
+						>
+							{'>'}
+						</button>
+						<button
+							className='next-btn'
+							onClick={() => setPageIndex(data.length - 1)}
+							disabled={pageIndex >= data.length - 1}
+						>
+							{'>>'}
+						</button>
+					</div>
+				)}
+			</section>
+		</main>
+	)
 }
 
 export default App
--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,9 @@
-const paginate = () => {}
+const PAGE_LENGTH = 10
+
+const paginate = (followers) => {
+	return Array.from({ length: followers.length / PAGE_LENGTH }, (_, index) =>
+		followers.slice(index * PAGE_LENGTH, index * PAGE_LENGTH + PAGE_LENGTH)
+	)
+}
 
 export default paginate
